377ec67487d88a4db9ace8158e418486433671e7
Eli Leyder <eli.leyder@gmail.com>
2023-12-10 22:36:26 +0100

    js and features

---
+++ 3. Bookmark landing page master/JS/script.js
@@ -0,0 +1,25 @@
+
+function changerFeatures(targetItemId, control) {
+    // Récupérer tous les items
+    let allItems = document.querySelectorAll('.features-items');
+
+    // Afficher l'item cible et cacher les autres
+    allItems.forEach(function(item) {
+        if (item.id === targetItemId) {
+        item.style.display = 'flex';
+        } else {
+        item.style.display = 'none';
+        }
+    });
+
+    // Supprimer la classe "active" de tous les contrôles
+    let allControls = document.querySelectorAll('.control');
+    allControls.forEach(function(ctrl) {
+        ctrl.classList.remove('active');
+    });
+
+    // Ajouter la classe "active" au contrôle cliqué
+    control.classList.add('active');
+}
+// Afficher le premier élément au chargement de la page
+changerFeatures('item-1', document.querySelector('.control.active'));
